a18d0e73c61426d9c43b6d59a3b4679c5c6332f0
geromarq <142598233+geromarq@users.noreply.github.com>
2024-06-17 10:41:42 -0300

    idk

--- TIPY1.1/funciones.php
+++ TIPY1.1/funciones.php
@@ -17,3 +17,37 @@ function getNombreFiesta()
         echo "<p>Parameters 'name' and 'age' are not set in the URL.</p>";
     }
 }
+function getNombreDJ()
+{
+    if (isset($_GET['idfiesta'])) {
+        $idFiesta = htmlspecialchars($_GET['idfiesta']); // htmlspecialchars() is used to avoid XSS attacks
+        $conexion = mysqli_connect("localhost", "root", "", "tipy") or die("Problemas con la base de datos");
+        $registros = mysqli_query($conexion, "SELECT nombre_Dj FROM djs WHERE id_fiesta = '$idFiesta' ") or die("Problemas en el select:" . mysqli_error($conexion));
+        if (mysqli_num_rows($registros)) {
+            return true;
+        } else if ($registros = null) {
+            return false;
+        }
+        mysqli_close($conexion);
+        echo "<h3>Hello, $idFiesta!</h3>";
+    } else {
+        echo "<p>No conec</p>";
+    }
+}
+function getNombreLocal()
+{
+    if (isset($_GET['idfiesta'])) {
+        $idFiesta = htmlspecialchars($_GET['idfiesta']); // htmlspecialchars() is used to avoid XSS attacks
+        $conexion = mysqli_connect("localhost", "root", "", "tipy") or die("Problemas con la base de datos");
+        $registros = mysqli_query($conexion, "SELECT nombre_local FROM djs WHERE id_fiesta = '$idFiesta' ") or die("Problemas en el select:" . mysqli_error($conexion));
+        if (mysqli_num_rows($registros)) {
+            return true;
+        } else if ($registros = null) {
+            return false;
+        }
+        mysqli_close($conexion);
+        echo "<h3>Hello, $idFiesta!</h3>";
+    } else {
+        echo "<p>Parameters 'name' and 'age' are not set in the URL.</p>";
+    }
+}
