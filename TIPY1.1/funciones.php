d81adc63f365cdfaf9c29d60f47237258abcc51f
geromarq <142598233+geromarq@users.noreply.github.com>
2024-06-17 02:34:21 -0300

    somethingsomething

--- TIPY1.1/funciones.php
+++ TIPY1.1/funciones.php
@@ -2,10 +2,11 @@
 
 function getNombreFiesta()
 {
+    var $paseValido = true;
     if (isset($_GET['idfiesta'])) {
         $idFiesta = htmlspecialchars($_GET['idfiesta']); 
         $conexion = mysqli_connect("localhost", "root", "", "tipy") or die("Problemas con la base de datos");
-        $registros = mysqli_query($conexion, "SELECT nombre_fiesta FROM fiestas WHERE id_fiesta = '$idFiesta' ") or die("Problemas en el select:" . mysqli_error($conexion));
+        $registros = mysqli_query($conexion, "SELECT nombre_fiesta FROM fiestas WHERE FiestaID = '$idFiesta' ") or die("no se encontro fiesta" . mysqli_error($conexion));
         if (mysqli_num_rows($registros)) {
             return true;
         } else if ($registros = null) {
@@ -14,6 +15,20 @@ function getNombreFiesta()
         mysqli_close($conexion);
         echo "<h3>Hello, $idFiesta!</h3>";
     } else {
-        echo "<p>Parameters 'name' and 'age' are not set in the URL.</p>";
+        echo "<p>No se encontro la fiesta.</p>"; 
+        var $paseValido = false;
     }
+    return $paseValido
 }
+function cargarSugerencia(){
+    $conexion = mysqli_connect("localhost", "root", "", "tipy") or die("Problemas con la base de datos");
+    $registros = mysqli_query($conexion, "INSERT INTO Solicitudes (UsuarioID, TipoSolicitud, TextoSolicitud, MontoPropina, Estado, FiestaID) VALUES 
+(1, 'Solicitud de Canción', 'Reproducir "Shape of You" de Ed Sheeran', 10.00, 'Pendiente', 1),
+(2, 'Solicitud de Género', 'Más música Hip-Hop', 5.00, 'Pendiente', 1);
+") or die("no se encontro fiesta" . mysqli_error($conexion));
+}
+
+
+//Verificar que el qr es valido si devuelve valido
+//el qr te dirige a una pagina que solo corre php para ejecutar esta funcion. Luego te redirige a una pagina 404 
+//
